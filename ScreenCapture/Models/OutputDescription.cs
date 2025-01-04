a5de9a1ea2bf755a2075aca56ede7d77b414ba58
Yotic <yoticcitoy@gmail.com>
2025-01-05 00:57:10 +0300

    Added tabs detection

--- ScreenCapture/Models/OutputDescription.cs
+++ ScreenCapture/Models/OutputDescription.cs
@@ -1,5 +1,4 @@
 ﻿using ScreenCapture.DirectXModels;
-using ScreenCapture.DirectXModels.Mathematic;
 using System.Runtime.InteropServices;
 
 namespace ScreenCapture;
@@ -7,7 +6,7 @@ namespace ScreenCapture;
 public unsafe struct OutputDescription
 {
     [FieldOffset(0x00)] public fixed char DeviceName[32];
-    [FieldOffset(0x40)] public Rectangle DesktopBounds;
+    [FieldOffset(0x40)] public DirectXModels.Mathematic.Rectangle DesktopBounds;
     [FieldOffset(0x50)] public bool IsAttachedToDesktop;
     [FieldOffset(0x54)] public DisplayModeRotation DisplayMode;
     [FieldOffset(0x58)] public nint MonitorHandle;
