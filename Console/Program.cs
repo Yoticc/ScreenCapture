4da57fecf2c00641952fc277875806fc2bce09ba
Yotic <yoticcitoy@gmail.com>
2025-06-09 22:08:35 +0300

    Added second sample

--- Console/Program.cs
+++ Console/Program.cs
@@ -28,3 +28,37 @@ unsafe
         else Console.WriteLine($"Failed to capture {counter}-th frame. HResult: {result}");
     }
 }
+
+unsafe
+{
+    var samplesDirectory = @"C:\samples";
+    Directory.CreateDirectory(samplesDirectory);
+
+    GraphicDevice device;
+    GraphicDevice.EnumDevice(&device, index: 0);
+
+    Screen screen;
+    Screen.EnumScreen(&screen, &device, index: 0);
+
+    Duplicator duplicator;
+    Duplicator.Create(&device, &screen, &duplicator);
+
+    int counter = 1;
+    Frame frame;
+    HResult result;
+    while (true)
+    {
+        if (result = duplicator.CaptureFrame(&frame))
+        {
+            frame.Bitmap.Save(Path.Combine(samplesDirectory, $"sample-{counter}.png"));
+            Console.WriteLine($"Captured and saved {counter}-th frame!");
+            counter++;
+        }
+        else if (result.NoResult)
+        {
+            // skip, there's no new frame
+            Thread.Sleep(25);
+        }
+        else Console.WriteLine($"Failed to capture {counter}-th frame. HResult: {result}");
+    }
+}
